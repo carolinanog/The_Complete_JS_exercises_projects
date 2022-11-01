a49a97a29ce2d37f4beb9d03ab44684268d2ab0b
Carolina Nogueira <carolina.tinen@alumni.usp.br>
2022-11-01 14:04:29 -0300

    commit 2nd test

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,11 @@
 let js = 'amazing';
-
 console.log(40 + 8 + 23 -10);
+
+console.log('Carol');
+console.log(30);
+
+let firstName = 'Carolina';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
