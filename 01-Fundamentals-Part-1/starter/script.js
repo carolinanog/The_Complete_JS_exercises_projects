be6b0181ad919b0bd9476efa165cccf6e023c28f
Carolina Nogueira <carolina.tinen@alumni.usp.br>
2022-11-01 15:01:57 -0300

    Assignment JavaScript Fundamentals Part 1 - LECTURE: Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -9,3 +9,31 @@ let firstName = 'Carolina';
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+let funct$ion = 27; //the dollar sign is allowed as special character in variable names
+let cat = 'KitKat';
+let oldestPet = cat;
+//Camel Case
+
+let PI = 3.1415; 
+//all uppercase variable names are reserved for constants;
+
+let job1 = 'teacher';
+let job2 = 'programmer';
+
+let myFirstJob = 'teacher';
+let myCurrentJob = 'coder';
+//clean code: to make sure variable names are descriptive, making it easy to understand what the value holds by simply looking at the names
+
+//Assignment JavaScript Fundamentals Part 1 - LECTURE: Values and Variables
+
+// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+
+const country = 'Brasil';
+const continent = 'América';
+const population = 214;
+
+// 2. Log their values to the console
+console.log(country);
+console.log(continent);
+console.log(population);
